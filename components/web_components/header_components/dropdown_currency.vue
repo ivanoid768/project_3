8b1820451020ce57dba0762d59645f8142b3e120
doppeV <doppeV@yandex.ru>
2018-12-10 23:31:14 +0300

    propper currency selection

--- components/web_components/header_components/dropdown_currency.vue
+++ components/web_components/header_components/dropdown_currency.vue
@@ -1,12 +1,12 @@
 <template>
    <div class="btn-group dropdown-currency">
      <button type="button" class="btn btn-light dropdown-toggle "  @click="dropToggle"    >
-       <i class="currency-icon btc"></i> {{currency}}
+       <i class="currency-icon " v-bind:class="currencyIcon"></i> {{currency}}
      </button>
   <div class="dropdown-menu" v-if="showDrop">
     <button class="dropdown-btn" @click="selectCurrency" data-currency="BTC"><i class="currency-icon btc"></i>BTC</button>
     <button class="dropdown-btn" @click="selectCurrency" data-currency="LTC" ><i class="currency-icon ltc"></i>LTC</button>
-    <button class="dropdown-btn" @click="selectCurrency" data-currency="DASH"><i class="currency-icon dsh"></i>DASH</button>
+    <button class="dropdown-btn" @click="selectCurrency" data-currency="DASH"><i class="currency-icon dash"></i>DASH</button>
   </div>
   </div>
 </template>
@@ -18,20 +18,23 @@
     data: function () {
       return (
         {
+          currencyIcon: "btc",
           currency: "BTC",
           showDrop: false,
         }
       )
-    } ,
+    },
     methods: {
       selectCurrency: function (e) {
         let currency = e.currentTarget.dataset.currency;
         this.currency = currency;
+        this.currencyIcon = currency.toLowerCase();
         this.showDrop = false;
       },
       dropToggle: function () {
         this.showDrop = !this.showDrop;
-      }
+      },
+
     }
     
   }
